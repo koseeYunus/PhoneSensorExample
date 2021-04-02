bfb828ad9f53ffd8d84d2700d29a8c572d64fcb8
Yunus Köse <kose.yunus.55@gmail.com>
2021-04-02 23:05:16 +0300

    Circular chart added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'dart:io';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
+import 'package:flutter_circular_chart_two/flutter_circular_chart_two.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.indigo,
       ),
       home: MyHomePage(title: 'Flutter Sensör Uygulaması'),
     );
@@ -30,36 +30,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  double _ax, _ay, _az;
-  double _uax, _uay, _uaz;
+
+  final GlobalKey<AnimatedCircularChartState> _chartKey = GlobalKey<AnimatedCircularChartState>();
+  final _chartSize = const Size(200.0, 200.0);
+  double value = 0;
+  Color labelColor = Colors.red[200];
+
+
   double _gx, _gy, _gz;
 
-  void _incrementCounter() {
+  int _gPoint = 0;
+
+  String _status="Hadi başlayalım.";
+
+  void _increment() {
     setState(() {
-      _counter+=5;
+      value += 1;
+      List<CircularStackEntry> data = _generateChartData(value);
+      _chartKey.currentState.updateData(data);
     });
   }
+
+  void _decrement() {
+    setState(() {
+      value -= 1;
+      List<CircularStackEntry> data = _generateChartData(value);
+      _chartKey.currentState.updateData(data);
+    });
+  }
+
+  List<CircularStackEntry> _generateChartData(double value) {
+    Color dialColor = Colors.deepOrangeAccent[200];
+
+    labelColor = dialColor;
+
+    List<CircularStackEntry> data = <CircularStackEntry>[
+      CircularStackEntry(
+        <CircularSegmentEntry>[
+          CircularSegmentEntry(
+            value,
+            dialColor,
+            rankKey: 'percentage',
+          )
+        ],
+        rankKey: 'percentage',
+      ),
+    ];
+
+    if (value > 100) {
+      labelColor = Colors.indigoAccent;
+
+      data.add(CircularStackEntry(
+        <CircularSegmentEntry>[
+          CircularSegmentEntry(
+            value - 100,
+            Colors.indigoAccent,
+            rankKey: 'percentage',
+          ),
+        ],
+        rankKey: 'percentage2',
+      ));
+    }
+
+    if (value > 200) {
+      labelColor = Colors.green[200];
+
+      data.add(CircularStackEntry(
+        <CircularSegmentEntry>[
+          CircularSegmentEntry(
+            value - 200,
+            Colors.green[200],
+            rankKey: 'percentage',
+          ),
+        ],
+        rankKey: 'percentage3',
+      ));
+    }
+
+    return data;
+  }
+
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    accelerometerEvents.listen((AccelerometerEvent event) {
-      setState(() {
-        _ax = event.x;
-        _ay = event.y;
-        _az = event.z;
-      });
-    }); //get the sensor data and set then to the data types
-
-    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-      setState(() {
-        _uax = event.x;
-        _uay = event.y;
-        _uaz = event.z;
-      });
-    });
 
     gyroscopeEvents.listen((GyroscopeEvent event) {
       setState(() {
@@ -67,70 +122,86 @@ class _MyHomePageState extends State<MyHomePage> {
         _gy = event.y;
         _gz = event.z;
       });
+      if (_gx > 1 || _gy > 1 || _gz > 1) {
+        _gPoint += 1;
+        _increment();
+        Future.delayed(const Duration(seconds: 2), () => "2");
+      }
     });
+    debugPrint(_gPoint.toString());
+    if(_gPoint>0){
+      _status="Daha yeni başladık.";
+    }
+    else if(_gPoint>100){
+      _status="Neredeyse bitirdik.";
+    }
+    else if(_gPoint>200){
+      _status="Son aşama.";
+    }
+    else{
+      _status="BUGUNKİ SPORUNU TAMAMLADIN TEBRİKLER.";
+    }
+
 
   }
 
   @override
   Widget build(BuildContext context) {
+    TextStyle _labelStyle = Theme.of(context).textTheme.headline.merge(TextStyle(color: labelColor));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Accelerometer Events listen',
-              style: Theme.of(context).textTheme.headline,
+
+            Container(
+              child: AnimatedCircularChart(
+                key: _chartKey,
+                size: _chartSize,
+                initialChartData: _generateChartData(value),
+                chartType: CircularChartType.Radial,
+                edgeStyle: SegmentEdgeStyle.round,
+                percentageValues: true,
+                holeLabel: '$value%',
+                labelStyle: _labelStyle,
+              ),
             ),
+
+            Text(
+              _status,
+              style: Theme.of(context).textTheme.subtitle,
+            ),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Text(
-                  _ax.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
+                RaisedButton(
+                  onPressed: _decrement,
+                  child: const Icon(Icons.remove),
+                  shape: const CircleBorder(),
+                  color: Colors.red[200],
+                  textColor: Colors.white,
                 ),
-                Text(
-                  _ay.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                Text(
-                  _az.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
+                RaisedButton(
+                  onPressed: _increment,
+                  child: const Icon(Icons.add),
+                  shape: const CircleBorder(),
+                  color: Colors.blue[200],
+                  textColor: Colors.white,
                 ),
               ],
             ),
             SizedBox(height: 50,),
+
             Text(
-              'User Accelerometer Events listen',
+              'Jiroskop Verileri',
               style: Theme.of(context).textTheme.headline,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Text(
-                  _uax.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                Text(
-                  _uay.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                Text(
-                  _uaz.toStringAsFixed(2),
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
-            SizedBox(height: 50,),
-            Text(
-              'Gyroscope Events listen',
-              style: Theme.of(context).textTheme.headline,
-            ),
+            SizedBox(height: 25,),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               mainAxisSize: MainAxisSize.max,
@@ -149,14 +220,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "Puan",
+              style: Theme.of(context).textTheme.headline,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              _gPoint.toString(),
+              style: Theme.of(context).textTheme.display1,
+            ),
 
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
